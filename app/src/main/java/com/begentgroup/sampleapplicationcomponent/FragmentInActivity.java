8e95fa4cd5b68bc1b77495de5a94e5a687b8f0ae
Yon Seungik <dongja94@gmail.com>
2016-07-21 13:39:03 +0900

    arguments

--- app/src/main/java/com/begentgroup/sampleapplicationcomponent/FragmentInActivity.java
+++ app/src/main/java/com/begentgroup/sampleapplicationcomponent/FragmentInActivity.java
@@ -1,12 +1,16 @@
 package com.begentgroup.sampleapplicationcomponent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class FragmentInActivity extends AppCompatActivity {
+
+    private static final String TAG_ONE = "one";
+    private static final String TAG_TWO = "two";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +21,15 @@ public class FragmentInActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentTransaction ft = getSupportFragmentManager()
-                        .beginTransaction();
-                OneFragment f = new OneFragment();
-                ft.replace(R.id.container, f);
-                ft.commit();
+                Fragment old = getSupportFragmentManager()
+                        .findFragmentByTag(TAG_ONE);
+                if (old == null) {
+                    FragmentTransaction ft = getSupportFragmentManager()
+                            .beginTransaction();
+                    OneFragment f = OneFragment.newInstance("input message");
+                    ft.replace(R.id.container, f, TAG_ONE);
+                    ft.commit();
+                }
             }
         });
 
@@ -29,11 +37,15 @@ public class FragmentInActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentTransaction ft = getSupportFragmentManager()
-                        .beginTransaction();
-                TwoFragment f = new TwoFragment();
-                ft.replace(R.id.container, f);
-                ft.commit();
+                Fragment old = getSupportFragmentManager()
+                        .findFragmentByTag(TAG_TWO);
+                if (old == null) {
+                    FragmentTransaction ft = getSupportFragmentManager()
+                            .beginTransaction();
+                    TwoFragment f = new TwoFragment();
+                    ft.replace(R.id.container, f, TAG_TWO);
+                    ft.commit();
+                }
             }
         });
     }
--- app/src/main/java/com/begentgroup/sampleapplicationcomponent/OneFragment.java
+++ app/src/main/java/com/begentgroup/sampleapplicationcomponent/OneFragment.java
@@ -2,6 +2,7 @@ package com.begentgroup.sampleapplicationcomponent;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,11 +17,29 @@ import android.widget.TextView;
  */
 public class OneFragment extends Fragment {
 
+    public static OneFragment newInstance(String input) {
+        OneFragment fragment = new OneFragment();
+        Bundle b = new Bundle();
+        b.putString(KEY_INPUT, input);
+        fragment.setArguments(b);
+        return fragment;
+    }
+
+    public static final String KEY_INPUT = "input";
 
     public OneFragment() {
         // Required empty public constructor
     }
 
+    String inputText;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle b = getArguments();
+        if (b != null) {
+            inputText = b.getString(KEY_INPUT);
+        }
+    }
 
     EditText inputView;
     TextView messageView;
@@ -30,6 +49,7 @@ public class OneFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_one, container, false);
         inputView = (EditText)view.findViewById(R.id.edit_input);
+        inputView.setText(inputText);
         messageView = (TextView)view.findViewById(R.id.text_message);
         Button btn = (Button)view.findViewById(R.id.btn_send);
         btn.setOnClickListener(new View.OnClickListener() {
